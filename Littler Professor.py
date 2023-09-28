69d9762ba35e80e42c64292d08ac534a39e4a276
Moisés Arruda <moises_arruda@outlook.com>
2023-09-28 10:45:59 -0300

    Comentários na função questao()

--- Littler Professor.py
+++ Littler Professor.py
@@ -25,6 +25,13 @@ def numeros_aleatorios(gerador):
     return n1, n2
 
 
+#def tratramento_erros():
+    """
+    Função para tratar os erros
+    :return:
+    """
+
+
 def dificuldade():
     """
     Essa função deve coletar o nível de dificuldade escolhido pelo usuário, retornar a mesma, e verificar
@@ -57,6 +64,7 @@ def dificuldade():
         except KeyboardInterrupt:
             #   Encerramento brusco do programa
             print('\nFinalizando o programa. Encerrando...')
+            #   Forçar o encerramento do programa
             sys.exit()
 
 
@@ -81,42 +89,67 @@ def calculos():
     return valor_1, valor_2, soma, subtracao, multiplicacao, divisao
 
 def tentativas_usuario():
-
+    """
+    Cria um loop de 10 tentativas do usuário. Para cada alternativa correta, ele irá reaproveitar as
+    variáveis acertos, erros de questão(), acrescentar conforme a resposta, e retornar a quantidade
+    de cada loop para as váriaveis dele. Quando estiver no último loop irá trazer a mensagem do if
+    """
+    #   Irá armazenar cada interação no loop e acrescentar conforme acertos
     corretas = 0
+    #   Irá armazenar cada interação no loop e acrescentar conforme erros
     erradas = 0
+    #   Loop para rodar 10 vezes
     for j in range(1,11):
+        #   Reaproveitando as variáveis de questão, a cada interação, armazena conforme a resposta.
         acertos, erros = questao()
+        #   Usuário acertando a resposta, acrescenta +1 para corretas
         corretas += acertos
+        #   Usuário errando a resposta, acrescenta +1 para erradas
         erradas += erros
+        #   Quando estiver no último loop, retorna uma mensagem com a pontuação do usuário
         if j == 10:
             print(f'Você teve {corretas} respostas certas.\n E {erradas} respostas erradas')
 
 def questao():
     """
-    Essa linha de código verifica se o calculo feito pelo usuário está correto
+    Essa linha de código verifica se o calculo feito pelo usuário está correto, assim como realiza
+    tratativa de erros
     """
     # Obter os valores armazenados nas váriaveis da função calculos
     valor_1, valor_2, soma, subtracao, multiplicacao, divisao = calculos()
+    #   A cara interação, o valor será acrescentado +1
     acertos = 0
+    #   A cara interação, o valor será acrescentado +1
     erros = 0
+    #   Enquanto a condição não for resolvida, continuará rodando
     while True:
         try:
             # Armazena a resposta do usuário
             calculo = int(input(f'Faça o cálculo de {valor_1} + {valor_2} : '))
+            #   Se a resposta dele for igual a variável de soma de calculos()
             if calculo == soma:
-                #   Se o usuário indicar um valor igual o da variável soma
+                #   Mensagem de acerto é retornada, acertos é acrescentado +1 e vai para a próxima pergunta
                 print('Você acertou, parabéns!')
                 acertos += 1
+                #   Encerrar essa questão
                 break
+            #   Se a resposta estiver incorreta
             else:
+                #   Retorna a mensagem
                 print('Resposta incorreta!')
+                #   Acrescenta +1 na variável erros
                 erros += 1
+                #   Encerra essa questão
                 break
+        #   Caso o usuário coloque algum valor não numérico, apresenta o erro e permanece na questão
         except ValueError:
             print('Esses não são valores numéricos. Tente novamente!')
+        #   Se o usuário desejar encerrar o programa, apresenta a mensagem
         except KeyboardInterrupt:
             print('\nPrograma finalizado. Encerrando...')
+            #   Função para forçar a saida do programa
             sys.exit()
+    #   Retorna os valores para cada chamada de função em partes diferentes do código
     return acertos, erros
 
 # Chama a função e realiza a apresentação do programa
